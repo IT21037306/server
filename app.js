01b4df4add6cb52e0865e352626d5f8f39539521
Ahamed Fahmi <it21037306@my.sliit.lk>
2024-09-24 13:35:12 +0530

    Fixed Low Risk Vulnerability : Sensitive Cookie in HTTPS Session Without 'Secure' Attribute (Test)
    Cookie has the Secure attribute set to false. Set it to true to protect the cookie from man-in-the-middle attacks.
    
    app.js
    
    Signed-off-by: Ahamed Fahmi <it21037306@my.sliit.lk>

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false },
+    cookie: { secure: false, httpOnly: true, sameSite: "strict" },
   })
 );
 
