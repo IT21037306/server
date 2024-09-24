f44e27b63c17298046b9df7d34e375f2f6b7439a
Ahamed Fahmi <it21037306@my.sliit.lk>
2024-09-24 12:10:18 +0530

    Fixed High Risk Vulnerability : Moved secret keys to .env file
    uploadImage.js file had secret keys hardcoded. This is a high risk vulnerability. So, I moved the secret keys to a .env file and added the .env file to the .gitignore file.
    
    Signed-off-by: Ahamed Fahmi <it21037306@my.sliit.lk>

--- uploadImage.js
+++ uploadImage.js
@@ -1,9 +1,9 @@
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: "dgpbzgp04",
-  api_key: "395256484813675",
-  api_secret: "0sWZmR4E5zKzJ5zr0k1gri29Swg"
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const opts = {
